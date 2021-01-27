7342fe15675e5ac756ba1af618e9623d05a7edef
Grzegorz <g.juzba@gmail.com>
2021-01-27 19:28:49 +0100

    add menu scroll

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 const burger = document.querySelector(".burger");
-const menu= document.querySelector(".menu")
+const menu= document.querySelector(".menu");
+const menuList=document.querySelector(".menu");
+
+
 
 burger.addEventListener("click", function(){
     menu.classList.toggle("active")
 } )
+
+menuList.addEventListener("click", function(){
+    menu.classList.remove("active")
+} )
+
+
+
+
+
