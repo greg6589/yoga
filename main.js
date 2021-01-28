d2b12859f7eee508a847de0cecd89da8215bc4f8
Grzegorz <g.juzba@gmail.com>
2021-01-28 09:55:25 +0100

    add new hideMenu function

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const burger = document.querySelector(".burger");
 const menu= document.querySelector(".menu");
-const menuList=document.querySelector(".menu");
-
 
 burger.addEventListener("click", function(){
     menu.classList.toggle("active")
 } )
 
-menuList.addEventListener("click", function(){
+const hideMenu=()=>{
+    console.log("działa");
     menu.classList.remove("active")
-} )
+}
+
+document.querySelectorAll(".menu li").forEach(item=>item.addEventListener("click", hideMenu))
